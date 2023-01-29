18b93b1b869ea9d3e241e2d70899ee468ce69038
Jonathan Dickert <jonathan.dickert@protonmail.com>
2023-01-29 00:37:33 -0500

    may have class macro initial dev done

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,45 +1,54 @@
 use proc_macro::TokenStream;
+use proc_macro2::Literal;
 use quote::quote;
-use syn::{ItemImpl, ItemStruct, PathArguments, Type};
+use syn::{parse_macro_input, Fields, ItemImpl, ItemStruct};
 
-#[proc_macro_derive(Class)]
+#[proc_macro_derive(Class, attributes(field))]
 pub fn derive_class(input: TokenStream) -> TokenStream {
-  let struct_def: ItemStruct = match syn::parse(input.clone()) {
-    Ok(item) => item,
-    Err(err) => return token_stream_with_error(input, err),
-  };
+  let struct_def = parse_macro_input!(input as ItemStruct);
 
-  let mut _getters = Vec::<()>::new();
-  let mut _setters = Vec::<()>::new();
+  let mut getters = Vec::new();
+  let mut setters = Vec::new();
 
-  for field in &struct_def.fields {
-    if let Some(seg) = field
-      .attrs
-      .iter()
-      .find_map(|attr| attr.path.segments.iter().find(|seg| seg.ident == "field"))
-    {
-      /* struct Foo {
-       *   #[field]
-       *   foo: String,
-       * }
-       */
-      if let PathArguments::Parenthesized(args) = &seg.arguments {
-        for arg_input in &args.inputs {
-          if let Type::Verbatim(_arg) = arg_input {
-            // match syn::parse(arg.into()) {
-            //   Ok(_) => todo!(),
-            //   Err(err) => return token_stream_with_error(input, syn::Error::new_spanned(arg, "not a valid field")),
-            // }
-          }
-        }
+  if let Fields::Named(fields) = &struct_def.fields {
+    /* struct Foo {
+     *   #[field]
+     *   foo: String,
+     * }
+     */
+    for field in &fields.named {
+      if field.attrs.iter().find(|attr| attr.path.is_ident("field")).is_some() {
+        let ident = field.ident.clone().unwrap();
+        getters.push(ident.clone());
+        setters.push(ident);
       }
     }
+  } else {
+    return TokenStream::from(syn::Error::new_spanned(struct_def.fields, "not a valid class field").to_compile_error());
   }
 
   let name = struct_def.ident;
+
+  let getter_strs = getters
+    .iter()
+    .map(|ident| Literal::string(&ident.to_string()))
+    .collect::<Vec<Literal>>();
   quote! {
     impl Class for #name {
+      fn get(&self, field: &str) -> Value {
+        match field {
+          #(#getter_strs => Value::from(&self.#getters),)*
+          _ => Value::default(),
+        }
+      }
 
+      fn set(&mut self, field: &str, value: Value) -> Result<(), Box<dyn std::error::Error>> {
+        match field {
+          #(#getter_strs => self.#getters = value.try_into()?,)*
+          _ => (),
+        }
+        Ok(())
+      }
     }
   }
   .into()
@@ -47,21 +56,18 @@ pub fn derive_class(input: TokenStream) -> TokenStream {
 
 #[proc_macro_attribute]
 pub fn class_body(_args: TokenStream, input: TokenStream) -> TokenStream {
-  let struct_impl: ItemImpl = match syn::parse(input.clone()) {
-    Ok(item) => item,
-    Err(err) => return token_stream_with_error(input, err),
-  };
+  let struct_impl = parse_macro_input!(input as ItemImpl);
 
   let me = struct_impl.self_ty;
 
   quote! {
     impl ClassBody for #me {
-      fn body
     }
   }
   .into()
 }
 
+#[allow(unused)]
 fn token_stream_with_error(mut tokens: TokenStream, error: syn::Error) -> TokenStream {
   tokens.extend(TokenStream::from(error.into_compile_error()));
   tokens
--- macros/tests/user.rs
+++ macros/tests/user.rs
@@ -3,3 +3,76 @@ fn user_tests() {
   let t = trybuild::TestCases::new();
   t.pass("tests/pass/*.rs")
 }
+
+#[cfg(test)]
+mod tests {
+  use macros::Class;
+  use std::error::Error;
+
+  trait Class {
+    fn get(&self, field: &str) -> Value;
+    fn set(&mut self, field: &str, value: Value) -> Result<(), Box<dyn Error>>;
+  }
+
+  #[derive(Debug)]
+  enum Value {
+    I32(i32),
+    F32(f32),
+    Nil,
+  }
+
+  impl From<&i32> for Value {
+    fn from(value: &i32) -> Self {
+      Self::I32(*value)
+    }
+  }
+
+  impl From<&f32> for Value {
+    fn from(value: &f32) -> Self {
+      Self::F32(*value)
+    }
+  }
+
+  impl TryFrom<Value> for i32 {
+    type Error = &'static str;
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+      if let Value::I32(i) = value {
+        Ok(i)
+      } else {
+        Err("")
+      }
+    }
+  }
+
+  impl TryFrom<Value> for f32 {
+    type Error = &'static str;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+      if let Value::F32(f) = value {
+        Ok(f)
+      } else {
+        Err("")
+      }
+    }
+  }
+
+  impl Default for Value {
+    fn default() -> Self {
+      Self::Nil
+    }
+  }
+
+  #[derive(Default, Class)]
+  struct Foo {
+    #[field]
+    foo: i32,
+
+    #[field]
+    foo2: f32,
+  }
+
+  #[test]
+  fn try_compile() {
+    Foo::default();
+  }
+}
