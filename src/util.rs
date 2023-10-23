f54af3f011dcfc7c727b7720b64f9d240a767d86
Jonathan Dickert <jonathan.dickert@protonmail.com>
2023-10-22 22:19:48 -0400

    cache works on linux

--- src/util.rs
+++ src/util.rs
@@ -1,14 +1,19 @@
 #[cfg(windows)]
 pub mod windows;
+#[cfg(windows)]
+pub use windows::*;
+#[cfg(windows)]
+pub type PlatformMetadata = WindowsMetadata;
+
+#[cfg(unix)]
+pub mod unix;
+#[cfg(unix)]
+pub use unix::*;
+#[cfg(unix)]
+pub type PlatformMetadata = UnixMetadata;
 
 use anyhow::Result;
 use std::path::Path;
-
-#[cfg(windows)]
-pub use windows::*;
-
-#[cfg(windows)]
-pub type PlatformMetadata = WindowsMetadata;
 
 pub trait FileMetadata {
   fn id_of(path: &Path) -> Result<FileIdType>;
